a7978d191be13c8da93f0a3467b17261d62492a6
athena272 <guilhermera272@gmail.com>
2022-07-29 15:27:32 -0300

    Update 003.py

--- 003.py
+++ 003.py
@@ -8,12 +8,23 @@ def bubbleSort(list):
                 temp = list[i]
                 list[i] = list[i+1]
                 list[i+1] = temp
-
-list = [5, 1, 4, 2, 8]
-bubbleSort(list)
-print(list)
+    
+    return list
 
 #reverse bubbleSort
+def inverseBubbleSort(list):
+    for passnum in range(len(list)-1,0,-1):
+        for i in range(passnum):
+            if list[i]<list[i+1]:
+                temp = list[i]
+                list[i] = list[i+1]
+                list[i+1] = temp
 
+    return list
+
+#main code
+list = [5, 1, 4, 2, 8]
+print(bubbleSort(list))
+print(inverseBubbleSort(list))
 
 #Link repositorio: https://github.com/athena272/BNE_TI
