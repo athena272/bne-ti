cbd3259e194d17826dcc0eb9e65f6b77928f3222
athena272 <guilhermera272@gmail.com>
2022-07-29 15:18:37 -0300

    Update 004.py

--- 004.py
+++ 004.py
@@ -1,10 +1,11 @@
 #Link repositorio: https://github.com/athena272/BNE_TI
 
-listaNum = input("informe os numeros do primeiro vetor separado por espaco: ").split()
+listaNum1 = input("informe os numeros do primeiro vetor separado por espaco: ").split()
 listaNum2 = input("informe os numeros do segundo vetor separado por espaco: ").split()
 
+#Gera a lista intercaldada
 listaVector = []
-for i, j in zip(listaNum, listaNum2):
+for i, j in zip(listaNum1, listaNum2):
   listaVector.append(int(i))
   listaVector.append(int(j))
 
