14383acafdc4a16c7005286e0f01682fad3069bd
athena272 <guilhermera272@gmail.com>
2022-09-08 13:09:32 -0300

    Create Program.cs

---
+++ logicaProg/Problema3/Problema3/ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿int a = 5, b = 10, c = 15, d = 20; // declaramos quatro variáveis do tipo int
+Console.WriteLine(a + d); // operação de soma
+Console.WriteLine(c - a); // operação de subtração
+Console.WriteLine(b * c); // operação de multiplicação
+Console.WriteLine(d / b); // operação de divisão
+Console.WriteLine(c % b); // operação de módulo (resto de divisão)
+Console.ReadKey();
