c720466c84246416552a32e2690c62758957b1e0
athena272 <guilhermera272@gmail.com>
2022-09-14 23:56:51 -0300

    Update Program.cs

--- logicaProg/Problema3/Problema3/ConsoleApp1/Program.cs
+++ logicaProg/Problema3/Problema3/ConsoleApp1/Program.cs
@@ -2,30 +2,42 @@
 
 namespace Sample
 {
-    class Problema3
+    class Problema6
     {
         public static void Main(string[] args)
         {
-            string userInput;
-            double valorPresente, taxaJuros, periodo, rendimento;
-            Console.Write("Informe o valor presente R$");
-            userInput = Console.ReadLine();
-            /* Converts to double type */
-            valorPresente = Convert.ToDouble(userInput);
 
-            Console.Write("Informe a taxa de juros: ");
-            userInput = Console.ReadLine();
-            /* Converts to double type */
-            taxaJuros = Convert.ToDouble(userInput);
+            double valorInicial,valorPresente, taxaJuros, rendimento, acumulado, saldo;
+            const int periodo = 8;
+
+            Console.Write("Informe o valor inicial R$");
+            valorInicial = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Informe a taxa de juros[%]: ");
+            taxaJuros = Convert.ToDouble(Console.ReadLine());
             taxaJuros = taxaJuros / 100;
 
-            Console.Write("Informe o período em meses: ");
-            userInput = Console.ReadLine();
-            /* Converts to double type */
-            periodo = Convert.ToDouble(userInput);
+            int i = 1;
+            //Iteracao acontecendo em um periodo de 8 meses e 10 dias
+            do
+            {
+                acumulado = valorInicial * Math.Pow((1 + taxaJuros), i);
+                valorPresente = acumulado;
 
-            rendimento = valorPresente * (Math.Pow((1 + taxaJuros), periodo));
-            Console.WriteLine($"O rendimento foi de R${Math.Round(rendimento, 2)}");
+                rendimento = acumulado - valorInicial;
+                saldo = acumulado - rendimento;
+
+                if(i == periodo)
+                {
+                    //Para encontrar quanto vale 10 dias
+                    int convertToDay = (1 + periodo) / 3; //10 dias
+                    acumulado = acumulado + (valorPresente * Math.Pow((1 + taxaJuros), convertToDay));
+                    Console.WriteLine($"O rendimento final foi de: R${Math.Round(acumulado, 3)}");
+
+                }
+                i++;
+
+            } while (i <= periodo);
 
         }
     }
