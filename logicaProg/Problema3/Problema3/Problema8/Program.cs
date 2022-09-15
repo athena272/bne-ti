f410f4dadeeeee05b6723a7b687bf00a5948e1ed
athena272 <guilhermera272@gmail.com>
2022-09-14 23:57:54 -0300

    Create Program.cs

---
+++ logicaProg/Problema3/Problema3/Problema8/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Sample
+{
+    class Problema8
+    {
+        public static void Main(string[] args)
+        {
+
+            double valorInicial, valorPresente, taxaJuros, rendimento, acumulado, saldo;
+            int periodo, periodoResgate;
+
+            Console.Write("Informe o valor inicial R$");
+            valorInicial = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Informe a taxa de juros[%]: ");
+            taxaJuros = Convert.ToDouble(Console.ReadLine());
+            taxaJuros = taxaJuros / 100;
+
+            Console.Write("Informe o período em meses: ");
+            periodo = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Informe em que mes deseja fazer uma resgate: ");
+            periodoResgate = Convert.ToInt32(Console.ReadLine());
+
+
+            int i = 1;
+            int mesAtual = 1;
+            
+            do
+            {
+                acumulado = valorInicial * Math.Pow((1 + taxaJuros), i);
+                
+                rendimento = acumulado - valorInicial;
+                Console.Write($"Rendimento do {i}º mes, R${rendimento}");
+
+                if (periodoResgate == periodo)
+                {
+                    Console.WriteLine("Chegou o mes escolhido para o resgate");
+                    Console.Write($"O valor de resgate no mes {periodoResgate}, foi de R${rendimento}");
+                    i = 1;
+                    mesAtual++;
+                    continue;
+
+                }
+                i++;
+                mesAtual++;
+
+            } while (mesAtual <= periodo);
+
+        }
+    }
+}
